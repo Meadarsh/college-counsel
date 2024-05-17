6f5fad67b00218612f7feda2a28567a877c1ab51
meadarsh <itsadarsh33@gmail.com>
2024-05-17 15:32:36 +0530

    g

--- src/databaseConnection/connect.js
+++ src/databaseConnection/connect.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 const connectDb = async () => {
     try {
-    await mongoose.connect(process.env.MONGO_URI);
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, // 30 seconds
+            socketTimeoutMS: 45000 // 45 seconds
+          };
+    await mongoose.connect(process.env.MONGO_URI,options);
         console.log("MongoDB Conneted");
     } catch (err) {
         console.error(err);
