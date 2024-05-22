fe46500bbd22fab2c4c79ccf85b4a2322a9d0128
meadarsh <itsadarsh33@gmail.com>
2024-05-22 18:55:34 +0530

    s

--- src/app/api/blog/getBlog/route.js
+++ src/app/api/blog/getBlog/route.js
@@ -1,33 +1,32 @@
+// src/app/api/blog/getBlog/route.js
 import connectDb from "@/databaseConnection/connect";
 import Blogs from "@/models/blog.model";
 import { NextResponse } from "next/server";
 
-export async function GET(req){
-    try{
-        await connectDb()
-        const Blog=await Blogs.find({},'title subtitle image upload_time')
-        const response = new NextResponse(JSON.stringify(Blog), {
-            headers: {
-              'Content-Type': 'application/json',
-              'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-              'Pragma': 'no-cache',
-              'Expires': '0',
-              'Surrogate-Control': 'no-store'
-            },
-          });
-          
-          return response;
-    }   
-    catch (err) {
+export async function GET() {
+  try {
+    await connectDb();
+    const blogs = await Blogs.find({}, 'title subtitle image upload_time');
+    const response = new NextResponse(JSON.stringify(blogs), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Surrogate-Control': 'no-store'
+      },
+    });
+    return response;
+  } catch (err) {
     console.log(err);
     return new NextResponse(JSON.stringify({ err, message: "Internal server error" }), {
-        headers: {
-          'Content-Type': 'application/json',
-          'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          'Pragma': 'no-cache',
-          'Expires': '0',
-          'Surrogate-Control': 'no-store'
-        },
-      });    }
-
-} 
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Surrogate-Control': 'no-store'
+      },
+    });
+  }
+}
