5ec56b69ceb6e4f730ad615d6c0c70563cc89b2f
meadarsh <itsadarsh33@gmail.com>
2024-06-01 12:05:40 +0530

    New

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,6 @@ module.exports = {
         name: "next-app",
         script: "node_modules/next/dist/bin/next",
         args: "start -p 3000",
-        env: {
-          NODE_ENV: "production",
-        },
       },
     ],
   };
