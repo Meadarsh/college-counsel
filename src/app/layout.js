194b1dafe7ba7df0c95640fc74683cfd2e1e8b5c
meadarsh <itsadarsh33@gmail.com>
2024-06-08 15:28:16 +0530

    s

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-674393N9L0"
+        strategy="afterInteractive"
+      />
+
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-674393N9L0');
+        `}
+      </Script>
      <Script id="messenger-widget-b" defer src="https://cdn.botpenguin.com/website-bot.js" >6647981f07af820cc774bacd,664797f8881a65150d0aa425</Script>
     
       <body className={frankRuhlLibre.className}>
