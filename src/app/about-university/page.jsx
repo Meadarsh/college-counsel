1ac08786b259c98e5bb9b6a002d9245f475e8b51
meadarsh <itsadarsh33@gmail.com>
2024-06-24 02:48:13 +0530

    d

--- src/app/about-university/page.jsx
+++ src/app/about-university/page.jsx
@@ -3,19 +3,19 @@ import { collegeLogo } from '../Data/collegeLogo'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const page = () => {
+const Page = () => {
   return (
     <div className=' flex flex-col mt-[8vh] lg:mt-[15vh] gap-4 items-center'>
-    <h1 className='lg:text-3xl font-bold'>We have tied-up with Top Universities like</h1>
-     <div className='flex flex-wrap gap-4 justify-center'>
-     {collegeLogo.map((e,ind)=>(<Link key={ind} href={e.route||'#'}>
-      <div  className='flex bg-white flex-col px-2 w-40 lg:w-52 h-24 rounded-lg justify-center items-center border border-primary'>
-      <Image height={50} width={80} src={e.logoimg} alt="College logo" className='w-auto h-auto mb-2' />
-      <p className=' leading-tight font-bold text-gray-600 text-sm '>{e.Offeredcourse} Courses</p>
-      <h1 className=' whitespace-nowrap text-[12px]'>{e.collegename}</h1>
-     </div></Link>))}</div>
-  </div>
+      <h1 className='lg:text-3xl font-bold'>We have tied-up with Top Universities like</h1>
+       <div className='flex flex-wrap gap-4 justify-center'>
+       {collegeLogo.map((e,ind)=>(<Link key={ind} href={e.route||'#'}>
+        <div  className='flex overflow-clip bg-white flex-col px-2 w-40 lg:w-52 h-28  rounded-lg justify-center lg:items-center border'>
+        <Image height={60} width={120} src={e.logoimg} alt="College logo" className='w-auto h-auto mb-2' />
+        <p className=' leading-tight font-bold text-gray-600 text-sm '>{e.Offeredcourse} Courses</p>
+        <h1 className=' whitespace-nowrap text-[14px]'>{e.collegename}</h1>
+       </div></Link>))}</div>
+    </div>
   )
 }
 
-export default page
+export default Page
