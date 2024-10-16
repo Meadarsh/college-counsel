10746ac5fa496bd0585616399284634a8c91a643
meadarsh <itsadarsh33@gmail.com>
2024-10-17 01:41:44 +0530

    Update

--- src/app/Data/footer.js
+++ src/app/Data/footer.js
@@ -1,9 +1,21 @@
 export const SocialLinks = [
-  { name: "Instagram", path: "https://www.instagram.com/collegecounselofficial",icon:'instagram' },
-  { name: "Whatsapp", path: "https://wa.me/+916392832171",icon:'whatsapp' },
-  { name: "Facebook", path: "https://www.facebook.com/share/j12tyceDXrtS2HWZ/?mibextid=LQQJ4d",icon:'facebook'},
-  { name: "Twitter", path: "https://x.com/CollegeCnslHQ",icon:'twitter'},
-  { name: "Linkedin", path: "https://www.linkedin.com/company/college-counsel",icon:'linkedin'},
+  {
+    name: "Instagram",
+    path: "https://www.instagram.com/collegecounselofficial",
+    icon: "instagram",
+  },
+  { name: "Whatsapp", path: "https://wa.me/+916392832171", icon: "whatsapp" },
+  {
+    name: "Facebook",
+    path: "https://www.facebook.com/share/j12tyceDXrtS2HWZ/?mibextid=LQQJ4d",
+    icon: "facebook",
+  },
+  { name: "Twitter", path: "https://x.com/CollegeCnslHQ", icon: "twitter" },
+  {
+    name: "Linkedin",
+    path: "https://www.linkedin.com/company/college-counsel",
+    icon: "linkedin",
+  },
 ];
 export const CourseList = [
   { name: "Online BA", path: "#" },
@@ -19,12 +31,13 @@ export const CourseList = [
 ];
 export const MoreList = [
   { name: "Blog", path: "/blog" },
+  { name: "News", path: "/news" },
   { name: "Careers", path: "#" },
   { name: "Verify certificate", path: "/certificate" },
   { name: "Online Counselling", path: "/apply" },
-]
+];
 
 export const Legal = [
   { name: "Privacy", path: "#" },
   { name: "Terms", path: "#" },
-]
+];
