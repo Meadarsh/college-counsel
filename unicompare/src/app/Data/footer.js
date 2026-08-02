1c7288b881b9077275296f62e5848e6c1a348bb0
remote_user <remote_user@synkly-server>
2026-08-02 05:01:03 +0000

    fix(footer): align course links with Courses section, remove Feed/Verify certificate/Online Counselling

--- unicompare/src/app/Data/footer.js
+++ unicompare/src/app/Data/footer.js
@@ -18,23 +18,20 @@ export const SocialLinks = [
   },
 ];
 export const CourseList = [
-  { name: "Online BA", path: "/blog/amity-online-ba" },
-  { name: "Online BBA", path: "/blog/uttaranchal-online-bba" },
-  { name: "Online BCA", path: "/blog/ignou-online-bca" },
-  { name: "Online Bcom", path: "/blog/manipal-online-b.com" },
+  { name: "Online BA", path: "/blog/smu-online-ba" },
+  { name: "Online BBA", path: "/blog/manipal-university-online-bba" },
+  { name: "Online BCA", path: "/blog/amrita-online-bca" },
+  { name: "Online Bcom", path: "/blog/shoolini-university-online-b.com" },
   { name: "Online Bsc", path: "#" },
   { name: "Online MA", path: "/blog/best-online-ma" },
-  { name: "Online MBA", path: "/blog/best-online-mca" },
-  { name: "Online MCA", path: "/blog/best-online-msc" },
+  { name: "Online MBA", path: "/blog/best-online-mba" },
+  { name: "Online MCA", path: "/blog/best-online-mca" },
   { name: "Online Mcom", path: "/blog/best-online-m.com" },
   { name: "Online Msc", path: "/blog/best-online-msc" },
 ];
 export const MoreList = [
   { name: "Blog", path: "/blog" },
   { name: "News", path: "/news" },
-  { name: "Feed", path: "/feed" },
-  { name: "Verify certificate", path: "/certificate" },
-  { name: "Online Counselling", path: "/apply" },
 ];
 
 export const Legal = [
